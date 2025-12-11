631a6a615b72dc6c67eb4d73f65deaefe5b4aa3b
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2025-12-11 08:10:04 +0200

    Day 11

--- AdventOfCode2025/Day11/Solver.cs
+++ AdventOfCode2025/Day11/Solver.cs
@@ -1,17 +1,79 @@
-﻿namespace AdventOfCode2025.Day11
+﻿using AdventOfCode2025.Utils;
+
+namespace AdventOfCode2025.Day11
 {
-    public class Solver(string input) : ISolver
+    public class Solver : ISolver
     {
-        private readonly string _input = input;
+        private readonly string _input;
+        private readonly Dictionary<string, List<string>> _machines = [];
+
+        public Solver(string input)
+        {
+            _input = input;
+
+            foreach (var line in _input.AsList())
+            {
+                var links = line.Split(' ');
+                _machines.Add(links.First()[..3], [.. links[1..]]);
+            }
+        }
 
         public string Part1()
         {
-            return "";
+            var paths = CountPaths("you", "out", []);
+            return paths.ToString();
         }
 
         public string Part2()
         {
-            return "";
+            var paths = CountPaths("svr", "out", ["fft", "dac"]);
+            return paths.ToString();
+        }
+
+        public long CountPaths(string startNode, string endNode, List<string> requiredNodes)
+        {
+            var requiredSet = new HashSet<string>(requiredNodes);
+
+            var cache = new Dictionary<(string Node, string RequiredNodes), long>();
+            var visited = new HashSet<string>();
+
+            long Dfs(string currentNode, HashSet<string> requiredNodes)
+            {
+                // Make local copy of hash set
+                var remaining = new HashSet<string>(requiredNodes);
+
+                // If end node reached, count only if all required nodes were also visited
+                if (currentNode == endNode)
+                    return remaining.Count == 0 ? 1 : 0;
+
+                var key = (currentNode, string.Join(",", remaining.Order()));
+
+                if (cache.TryGetValue(key, out long cached))
+                    return cached;
+
+                // Discard cyclic paths
+                if (visited.Contains(currentNode))
+                    return 0;
+
+                visited.Add(currentNode);
+
+                long total = 0;
+
+                var removed = remaining.Remove(currentNode);
+
+                foreach (var next in _machines[currentNode])
+                {
+                    total += Dfs(next, remaining);
+                }
+
+                visited.Remove(currentNode);
+
+                cache[key] = total;
+
+                return total;
+            }
+
+            return Dfs(startNode, requiredSet);
         }
     }
 }
--- AdventOfCode2025Tests/Day11/Day11Tests.cs
+++ AdventOfCode2025Tests/Day11/Day11Tests.cs
@@ -12,16 +12,16 @@ namespace AdventOfCode2025.Tests.Day11
             Solver solver = new(File.ReadAllText($"Day11\\sample.txt"));
             var rsp = solver.Part1();
 
-            Assert.That(rsp, Is.EqualTo(""));
+            Assert.That(rsp, Is.EqualTo("5"));
         }
 
         [Test()]
         public void Part2Test()
         {
-            Solver solver = new(File.ReadAllText($"Day11\\sample.txt"));
+            Solver solver = new(File.ReadAllText($"Day11\\sample2.txt"));
             var rsp = solver.Part2();
 
-            Assert.That(rsp, Is.EqualTo(""));
+            Assert.That(rsp, Is.EqualTo("2"));
         }
     }
 }
